56288046c806286225f67db4061f54144daa6cfc
mahume <michael.a.hume@gmail.com>
2019-06-19 15:53:40 -0600

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# RPG Game
+# Star Wars RPG
+
+[Homepage](./src/assets/images/homepage.png)
+
+## About
+
+Role playing game somewhere in a galaxy far far away.
+
+## Getting Started
+
+1.  Select player.
+2.  Select opponent.
+3.  Once in their fighting positions, click "Attack"
+4.  As you trade blows your "Attack" will increase while your "Health" decreases.
+5.  If you defeat your opponent, the opponent card will disappear.
+6.  Repeat from step #2 until no opponents remain.
+
+## Technologies
+
+Built using using core front-end technologies. Basic structure using HTML5. Layout and styling done with Bootstrap4's grid system and CSS3. Functionality is written with a combination of jQuery and vanilla JavaScript.
+
+- HTML5
+- CSS3
+  - Bootstrap4
+- JavaScript
+  - jQuery
+
+
+## Author
+
+[Mike Hume](https://mahume.github.io/)
--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,71 @@
-let whichCharacterPlayer; var whichCharacterOpponent; var playerReady; var 
-opponentReady;
-let opponentDefeatedArr = [];
-let skywalker = {
+var whichCharacterPlayer;
+var whichCharacterOpponent;
+var playerReady;
+var opponentReady;
+var opponentDefeatedArr = [];
+var skywalker = {
   health: 225,
   attack: 5,
   'counter attack': 10,
   playerSelected: false,
   opponentSelected: false,
-  displayStats: function () {
+  displayStats: function() {
     document.querySelector('#skywalkerHealthBadge').textContent = this.health;
     document.querySelector('#skywalkerAttackBadge').textContent = this.attack;
   },
 };
-let kenobi = {
+var kenobi = {
   health: 190,
   attack: 10,
   'counter attack': 20,
   playerSelected: false,
   opponentSelected: false,
-  displayStats: function () {
+  displayStats: function() {
     document.querySelector('#kenobiHealthBadge').textContent = this.health;
     document.querySelector('#kenobiAttackBadge').textContent = this.attack;
   },
 };
-let vader = {
+var vader = {
   health: 380,
   attack: 30,
   'counter attack': 40,
   playerSelected: false,
-  opponentSelected: false, 
-displayStats: function () {
+  opponentSelected: false,
+  displayStats: function() {
     document.querySelector('#vaderHealthBadge').textContent = this.health;
     document.querySelector('#vaderAttackBadge').textContent = this.attack;
   },
 };
-let sidious = {
+var sidious = {
   health: 350,
   attack: 25,
   'counter attack': 30,
   playerSelected: false,
   opponentSelected: false,
-  displayStats: function () {
+  displayStats: function() {
     document.querySelector('#sidiousHealthBadge').textContent = this.health;
     document.querySelector('#sidiousAttackBadge').textContent = this.attack;
   },
 };
 
-function reset() {
+var reset = function() {
   skywalker.displayStats();
   kenobi.displayStats();
   vader.displayStats();
   sidious.displayStats();
   playerReady = false;
   opponentReady = false;
-}
+};
 reset();
 
-function gameOver() {
+var gameOver = function() {
   if (opponentDefeatedArr.length === 3) {
     alert('You win!');
   }
-}
+};
 
 // Hover Effect
-$('img').on('mouseover', function () {
+$('img').on('mouseover', function() {
   if (!playerReady) {
     $(this).addClass('choosePlayer');
     if ($('img').hasClass('playerSelected')) {
@@ -76,7 +78,7 @@ $('img').on('mouseover', function () {
     }
   }
 });
-$('img').on('mouseleave', function () {
+$('img').on('mouseleave', function() {
   if (!playerReady) {
     $(this).removeClass('choosePlayer');
   } else {
@@ -85,15 +87,20 @@ $('img').on('mouseleave', function () {
 });
 
 // Click to Choose Player & Opponent
-$('img').on('click', function () {
+$('img').on('click', function() {
   if (!playerReady) {
     if (!$('img').hasClass('playerSelected')) {
       $(this).addClass('playerSelected');
-      $(this).siblings('ul').children('li:nth-child(3)').addClass('attackButton');
-      $(this).parent().slideUp('slow', function () {
-        $(this).appendTo('#playerFightArea');
-        $(this).slideDown('slow');
-      });
+      $(this)
+        .siblings('ul')
+        .children('li:nth-child(3)')
+        .addClass('attackButton');
+      $(this)
+        .parent()
+        .slideUp('slow', function() {
+          $(this).appendTo('#playerFightArea');
+          $(this).slideDown('slow');
+        });
       playerReady = true;
     } else {
       $(this).removeClass('playerSelected');
@@ -108,10 +115,12 @@ $('img').on('click', function () {
   } else {
     if (!$('img').hasClass('opponentSelected')) {
       $(this).addClass('opponentSelected');
-      $(this).parent().slideUp('slow', function () {
-        $(this).appendTo('#opponentFightArea');
-        $(this).slideDown('slow');
-      });
+      $(this)
+        .parent()
+        .slideUp('slow', function() {
+          $(this).appendTo('#opponentFightArea');
+          $(this).slideDown('slow');
+        });
       opponentReady = true;
     } else {
       $(this).removeClass('opponentSelected');
@@ -127,26 +136,33 @@ $('img').on('click', function () {
   }
 });
 // Game Play
-function battle() {
-  let attackButton = $('<button id="attackButton" class="btn btn-primary btn-lg d-block">Attack</button>');
+var battle = function() {
+  var attackButton = $(
+    '<button id="attackButton" class="btn btn-primary btn-lg d-block">Attack</button>'
+  );
   $('.attackButton').replaceWith(attackButton);
 
-  $('#attackButton').on('click', () => {
-        window[whichCharacterOpponent]['health'] -= window[whichCharacterPlayer]['attack'];
-        window[whichCharacterPlayer]['attack'] += window[whichCharacterPlayer]['attack'];
-        window[whichCharacterPlayer]['health'] -= window[whichCharacterOpponent]['counter attack'];
-        
-        window[whichCharacterPlayer].displayStats();
-        window[whichCharacterOpponent].displayStats();
+  $('#attackButton').on('click', function() {
+    window[whichCharacterOpponent].health -=
+      window[whichCharacterPlayer].attack;
+    window[whichCharacterPlayer].attack += window[whichCharacterPlayer].attack;
+    window[whichCharacterPlayer].health -=
+      window[whichCharacterOpponent]['counter attack'];
 
-        if (window[whichCharacterPlayer].health <= 0) {
-            alert('player loses');
-            location.reload();
-        } else if (window[whichCharacterOpponent].health <= 0) {
-            $('.opponentSelected').parent().remove();
-            opponentDefeatedArr.push(window[whichCharacterOpponent]);
-            gameOver();
-            opponentReady = false;
-        }          
-    });
-}
+
+    window[whichCharacterPlayer].displayStats();
+    window[whichCharacterOpponent].displayStats();
+
+    if (window[whichCharacterPlayer].health <= 0) {
+      alert('player loses');
+      location.reload();
+    } else if (window[whichCharacterOpponent].health <= 0) {
+      $('.opponentSelected')
+        .parent()
+        .remove();
+      opponentDefeatedArr.push(window[whichCharacterOpponent]);
+      gameOver();
+      opponentReady = false;
+    }
+  });
+};
